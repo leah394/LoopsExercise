a2e047a1a4347b080d2a4f0ed4a7771acb67dddf
Maleah Walden <maleah13walden@gmail.com>
2020-10-23 14:01:28 -0500

    Completed Exercise

---
+++ loopsExercise.js
@@ -0,0 +1,16 @@
+for (i = 1; i < 8; i++){
+    console.log(i);
+}
+
+for(i = 6; i <= 26; i += 4){
+    console.log(i);
+}
+
+const wizards = [`Harry Potter`, `Hermione Granger`, `Ron Weasley`];
+console.log(wizards);
+
+let harryPotterMovies = 0;
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+  }
